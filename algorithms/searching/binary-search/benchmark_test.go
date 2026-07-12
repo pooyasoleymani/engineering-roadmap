4a3bb7d4f8f00bc08601d63d92b6c5aadee62ad8
pouya <p.soleimaninejad@shpdco.ir>
2026-07-12 15:49:37 +0330

    [MOD] change some typo bugs

--- algorithms/searching/binary-search/benchmark_test.go
+++ algorithms/searching/binary-search/benchmark_test.go
@@ -2,9 +2,16 @@ package search
 
 import "testing"
 
-func BenchmarBinarySerach(b *testing.B) {
+func BenchmarkBinarySerach(b *testing.B) {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for b.Loop() {
 		BinarySearch(10, arr)
+	}
+}
+
+func BenchmarkBinarySerachRecursive(b *testing.B) {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for b.Loop() {
+		BinarySearchRecursive(10, 0, len(arr), arr)
 	}
 }
