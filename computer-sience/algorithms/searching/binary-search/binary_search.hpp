48c550fa184096e7277616ee0e2f283b5d8548e4
pouya <p.soleimaninejad@shpdco.ir>
2026-07-26 10:26:23 +0330

    vault backup: 2026-07-26 10:26:23

--- computer-sience/algorithms/searching/binary-search/binary_search.hpp
+++ computer-sience/algorithms/searching/binary-search/binary_search.hpp
@@ -1,6 +1,26 @@
-#include <algorithm>
+#pragma once
+#include <vector>
 
 
-int main() {
-    
+namespace Search {
+
+    template<typename T>
+    constexpr int lower_bound(int target, std::vector<T> arr) {
+         int res{arr.size()};
+         int low{0};
+         int high{res -1};
+
+        while low <= high {
+            int mid {low + (high - low)/2 };
+
+            if (target < arr[mid]) {
+                res = mid;
+                high = mid -1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
 }
---
+++ computer-sience/algorithms/searching/binary-search/cpp_tests/binary_search.cpp
@@ -0,0 +1,8 @@
+#include "binary_search.hpp"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("binary_search", "[lower_bound]") {
+    std::vector<int> arr {1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10}; 
+    REQUIRE(Search::lower_bound(6, arr) == 5);
+}
+
