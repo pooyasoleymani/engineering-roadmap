6602b2768f927b0f50693545583428aa481fc49c
pooyasoleymani <pooyasoleymaninejad@gmail.com>
2026-08-02 00:07:08 +0330

    vault backup: 2026-08-02 00:07:08

---
+++ computer-sience/algorithms/sorting/cpp_tests/sorting_test.cpp
@@ -0,0 +1,12 @@
+#include <catch2/catch_test_macros.hpp>
+#include <vector>
+#include "sorting.h"
+
+
+TEST_CASE("Selection sort", "[sort-methods]") {
+    std::vector<int> arr {1,23,56,45,23,5,6,7,2,22,33,33,55};
+    std::vector<int> sorted{1, 2, 5, 6, 7, 22, 23, 23, 33, 33, 45, 55, 56};
+
+    SelectionSort(arr);
+    REQUIRE(arr == sorted);
+}
--- computer-sience/algorithms/sorting/sorting.cpp
+++ computer-sience/algorithms/sorting/sorting.cpp
@@ -1,11 +1,14 @@
+#include "sorting.h"
 #include <vector>
 
 
 template <typename T>
-void swap(std::size_t i, std::size_t j) 
+void swap(std::size_t i, std::size_t j, std::vector<T>& arr) 
 {
-    auto temp{};
-    
+    std::vector<T> temp;
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
 
@@ -15,14 +18,32 @@ void SelectionSort(std::vector<T> arr)
     auto n = arr.size();
     for (auto i=0;i<n;i++) 
     {
-        MinIdx = i;
+        auto MinIdx = i;
         for (auto j=0;j<n-1;j++) 
         {
-            if arr[MinIdx] > arr[j] 
+            if (arr[MinIdx] > arr[j]) 
             {
                 MinIdx = j;
             }
         }
-        swap(i, MinIdx);
+        swap(i, MinIdx, arr);
+    }
+}
+
+
+template <typename T>
+void BubbleSort(std::vector<T> arr)
+{
+    auto n = arr.size();
+    for (auto i=0;i<n-1;i++) 
+    {
+        for (auto j=i;j<n-1;j++) 
+        {
+            if (arr[j] > arr[j+1] )
+            {
+                swap(i, j, arr);
+            }
+        }
+        
     }
 }
