c96fd708ef8fb36a7c178dfdff466146be4dab30
pooyasoleymani <pooyasoleymaninejad@gmail.com>
2026-08-17 20:41:19 +0330

    vault backup: 2026-08-17 20:41:19

--- computer-sience/algorithms/sorting/heap_sort.py
+++ computer-sience/algorithms/sorting/heap_sort.py
@@ -4,16 +4,21 @@ T = TypeVar("T")
 
 class MinHeap:
     def __init__(self, data: List[T]) -> None:
-        self.data: List[T] = data or []
+        self.data: List[T]
+        
+        self.size = len(data)
     
     def _swap(self ,i: int, j: int) -> None:
         self.data[i], self.data[j] = self.data[j], self.data[i]
     
     def _shift_up(self, index: int) -> None:
-        parent = (index -1) // 2
-        if self.data[parent] < self.data[index]:
+        if index < 1:
+            return
+        
+        elif parent := (index -1) // 2 and self.data[parent] < self.data[index]:
             self._swap(parent, index)
-            
+            return
+        
     def _shift_down(self, index: int) -> None:
         left = 2 * index + 1
         rigth = 2 * index + 2
@@ -26,4 +31,9 @@ class MinHeap:
             return
         
     def push(self, item: T) -> None:
-        pass
+        self.data.append(item)
+    
+    def heapify(self, ):
+        
+        for item in self.data:
+            
