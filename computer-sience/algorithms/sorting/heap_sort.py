ac1b6496d01fcaa1bd492ce417e2cf7c1b889cc6
pooyasoleymani <pooyasoleymaninejad@gmail.com>
2026-08-18 13:37:22 +0330

    vault backup: 2026-08-18 13:37:22

--- computer-sience/algorithms/sorting/heap_sort.py
+++ computer-sience/algorithms/sorting/heap_sort.py
@@ -5,8 +5,8 @@ T = TypeVar("T")
 class MinHeap:
     def __init__(self, data: List[T]) -> None:
         self.data: List[T]
-        
-        self.size = len(data)
+        for item in data:
+            self.push(item)    
     
     def _swap(self ,i: int, j: int) -> None:
         self.data[i], self.data[j] = self.data[j], self.data[i]
@@ -14,26 +14,39 @@ class MinHeap:
     def _shift_up(self, index: int) -> None:
         if index < 1:
             return
-        
         elif parent := (index -1) // 2 and self.data[parent] < self.data[index]:
             self._swap(parent, index)
-            return
         
     def _shift_down(self, index: int) -> None:
         left = 2 * index + 1
         rigth = 2 * index + 2
         
-        if self.data[index] > self.data[left]:
-            self._swap(index, left)
-            return
-        elif self.data[index] > self.data[rigth]:
-            self._swap(index, rigth)
-            return
+        largest = index
+        
+        if len(self.data) > left and self.data[largest] > self.data[left]:
+            largest - index
+        elif len(self.data) > rigth and self.data[largest] > self.data[rigth]:
+            largest = rigth
+        
+        if largest != index:
+            self._swap(largest, index)
         
     def push(self, item: T) -> None:
         self.data.append(item)
+        self._shift_down(len(self.data) - 1)
     
-    def heapify(self, ):
+    def peek(self) -> T:
+        return self.data[0]
+    
+    def pop(self) -> T:
+        size = len(self.data)
+        if size < 1:
+            return
+        # swap end item with minimum item
+        self._swap(0, size - 1)
+        # get minimum item
+        minimum = self.data.pop()
+        # shift down root 
+        self._shift_down(size - 1)
         
-        for item in self.data:
-            
+        return minimum
